a15e09623e3d446b0ab2cf472ffa7e0640a8f4fc
Pratik Neupane <pratikneupane1999@gmail.com>
2023-04-14 17:07:53 +0545

    feat: Added Grud Functionality for single post and Postman collection for easier API COnsuming

--- src/controllers/postController.ts
+++ src/controllers/postController.ts
@@ -31,3 +31,62 @@ export const createPost = async (req: Request, res: Response) => {
     res.status(500).send("Server Error");
   }
 };
+
+// GET a post by id
+export const getPostById = async (req: Request, res: Response) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    res.json(post);
+  } catch (err: any) {
+    console.error(err.message);
+    if (err.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    res.status(500).send("Server Error");
+  }
+};
+
+// PUT update post by ID
+export const updatePostById = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const { imageUrl, title, content } = req.body;
+
+    const updatedPost = await Post.findByIdAndUpdate(
+      id,
+      { imageUrl, title, content },
+      { new: true }
+    );
+
+    if (!updatedPost) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+
+    res.json(updatedPost);
+  } catch (err: any) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+};
+
+// DELETE post by id
+
+export const deletePostById = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const post = await Post.findById(id);
+
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+
+    await Post.deleteOne({ _id: id });
+    res.json({ msg: "Post deleted" });
+  } catch (err: any) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+};
--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -1,11 +1,22 @@
 import express from "express";
-import { getAllPosts, createPost } from "../controllers/postController";
+import { Request, Response } from "express";
+import { getAllPosts, createPost, getPostById, updatePostById, deletePostById } from "../controllers/postController";
 
 const router = express.Router();
 
 // GET all posts
-router.get("/", getAllPosts);
-// POST a new post
-router.post("/", createPost);
+router.get('/', getAllPosts);
+
+// GET single post by id
+router.get('/:id', getPostById);
+
+// POST create a new post
+router.post('/', createPost);
+
+// PUT update a post by id
+router.put('/:id', updatePostById);
+
+// DELETE delete a post by id
+router.delete('/:id', deletePostById);
 
 export default router;
